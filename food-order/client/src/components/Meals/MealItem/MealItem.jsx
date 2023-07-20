0e889ce9d8ec1cae8c0c39f865bf109362dd47d9
Audie Milson <milson.audie@gmail.com>
2023-07-20 22:28:41 +0700

    Add To Cart functionality

--- food-order/client/src/components/Meals/MealItem/MealItem.jsx
+++ food-order/client/src/components/Meals/MealItem/MealItem.jsx
@@ -2,9 +2,20 @@ import PropTypes from "prop-types";
 import classes from "./MealItem.module.css";
 
 import MealItemForm from "./MealItemForm";
+import { useContext } from "react";
+import CartContext from "../../../store/CartContext";
 
 const MealItem = (props) => {
+  const ctx = useContext(CartContext);
   const price = `$ ${props.price.toFixed(2)}`;
+  const addToCartHandler = (amount) => {
+    ctx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
   return (
     <li className={classes.meal}>
       <div>
@@ -13,13 +24,14 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
 };
 
 MealItem.propTypes = {
+  id: PropTypes.string,
   name: PropTypes.string,
   description: PropTypes.string,
   price: PropTypes.number,
--- food-order/client/src/components/UI/Input.jsx
+++ food-order/client/src/components/UI/Input.jsx
@@ -1,18 +1,21 @@
+import React from "react";
 import PropTypes from "prop-types";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input?.id}>{props.label}</label>
-      <input {...props.input} /> {/* use object destructure as attribute to element */}
+      <input ref={ref} {...props.input} /> {/* use object destructure as attribute to element */}
     </div>
   );
-};
+});
 
 Input.propTypes = {
   input: PropTypes.object,
   label: PropTypes.string,
 };
+
+Input.displayName = "Input";
 
 export default Input;
