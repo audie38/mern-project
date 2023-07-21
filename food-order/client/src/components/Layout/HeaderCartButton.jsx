ed1ee3e42d9cf76f8bbf312436331ea2b53f0fc3
Audie Milson <milson.audie@gmail.com>
2023-07-21 19:57:31 +0700

    Add Cart Button Animation

--- food-order/client/src/components/Layout/HeaderCartButton.jsx
+++ food-order/client/src/components/Layout/HeaderCartButton.jsx
@@ -1,16 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import CartContext from "../../store/CartContext";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [highlight, setHighlight] = useState(false);
   const ctx = useContext(CartContext);
-  const numOfCartItems = ctx.items.reduce((currNum, item) => {
+  const { items } = ctx;
+  const numOfCartItems = items.reduce((currNum, item) => {
     return currNum + item.amount;
   }, 0);
+  const btnClasses = `${classes.button} ${highlight ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setHighlight(true);
+    const timer = setTimeout(() => {
+      setHighlight(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <i className={`fa-solid fa-cart-shopping ${classes.icon}`}></i>
       <span>Your Cart</span>
       <span className={classes.badge}>{numOfCartItems}</span>
