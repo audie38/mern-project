681ea2996c9f0bb7ddba6fc0864c1a7955be12a4
Audie Milson <milson.audie@gmail.com>
2023-08-08 22:02:45 +0700

    Add onBlurHandler to Search Filter

--- react-keep/client/src/components/note/NoteFilter.jsx
+++ react-keep/client/src/components/note/NoteFilter.jsx
@@ -5,6 +5,8 @@ import { searchNotes } from "../../store/note/noteActions";
 const NoteFilter = () => {
   const dispatch = useDispatch();
   const [searchQuery, setSearchQuery] = useState("");
+  const [searchBlur, setSearchBlur] = useState(false);
+
   const setSearchQueryHandler = (event) => {
     setSearchQuery(event.target.value);
   };
@@ -14,14 +16,20 @@ const NoteFilter = () => {
     dispatch(searchNotes(searchQuery));
   };
 
+  const onBlurHandler = () => {
+    setSearchBlur(true);
+  };
+
   useEffect(() => {
-    dispatch(searchNotes(searchQuery));
-  }, [dispatch, searchQuery]);
+    if (searchBlur) {
+      dispatch(searchNotes(searchQuery));
+    }
+  }, [dispatch, searchQuery, searchBlur]);
 
   return (
     <div className="d-flex justify-content-center align-items-center">
       <form onSubmit={onSearchHandler} className="w-100 mx-auto" role="search">
-        <input className="form-control me-2" value={searchQuery} onChange={setSearchQueryHandler} type="search" placeholder="Search" aria-label="Search" />
+        <input className="form-control me-2" value={searchQuery} onChange={setSearchQueryHandler} onBlur={onBlurHandler} type="search" placeholder="Search" aria-label="Search" />
       </form>
     </div>
   );
